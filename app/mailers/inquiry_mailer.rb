481daed2bfb54e3715c474dd9effe6b9935a90ab
kyohhh <khnm0322@gmail.com>
2021-06-08 10:21:55 +0900

    メール本文のテンプレートの設定

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,6 +2,6 @@ class InquiryMailer < ApplicationMailer
   def received_email(user)
     @user = user
     mail to: @user.email,
-         subject: "【#{@user.name}さま、お知らせがあります】"
+         subject: "【#{@user.name}さまに日頃からの感謝についてお知らせがあります】"
   end
 end
